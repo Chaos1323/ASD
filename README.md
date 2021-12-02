92e9d3f73c9066e2a554c0342fffe6196ccb6352
chaos <chaos.void.beaver@gmail.com>
2021-12-02 16:15:32 +0800

    add project files

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
 # ASD
+
 Android Smali bytecode Debugger
 
-# Features
-Smali code debugging
-Line breakpoints
-Display variables
-Modify variable's value
+## Features
+
+ - Smali code debugging
+ - Line breakpoints
+ - Display variables
+ - Modify variable's value
+
+## Requirements
+
+ - adb tool should be installed
+ - the adb tool path should be added into the system EV
+
+## Extension Settings
+
+Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+
+For example:
+
+This extension contributes the following settings:
+
+* `myExtension.enable`: enable/disable this extension
+* `myExtension.thing`: set to `blah` to do something
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+//@ts-check
+
+'use strict';
+
+const path = require('path');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+
+  entry: { // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    extension : './src/extension.ts',
+    debugAdapter : './src/DebugMain.ts'
+  },
+  output: {
+    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]',
+    sourceMapFilename: '[name].js.map'
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    // modules added here also need to be added in the .vsceignore file
+  },
+  resolve: {
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    minimize: false,
+  }
+};
+module.exports = config;
